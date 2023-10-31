c3ec0425e6d4852b280e2ec8a0ed7f70720692ce
mahdmohsin54 <mahdmohsin54@gmail.com>
2023-10-31 18:40:47 -0500

    Final Submission

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import express from "express";
 import cors from "cors";
-import { flightsSeedData } from "./flightsData";
+import { Flight, flightsSeedData } from "./flightsData";
 
 const app = express();
 
@@ -18,4 +18,42 @@ app.get("/", (req, res) => {
   res.send({ message: "Hello World!" });
 });
 
-app.listen(3030, () => console.log("Server running on port 3030"));
+app.get("/flights", (req, res) => {
+  res.send(flights);
+});
+
+app.post("/flights", (req, res) => {
+  const newFlight = req.body;
+  flights.push(newFlight);
+  res.send(flights);
+});
+
+app.get("/flights/:flightNumber", (req, res) => {
+  const flightNumber = req.params.flightNumber;
+  const flight = flights.find(
+    (flight: Flight) => flight.flightNumber === flightNumber
+  );
+  res.send(flight);
+});
+
+app.patch("/flights/arrivalTime/:id", (req, res) => {
+  const flightId = req.params.id;
+  const updatedFlightArrivalTime = req.body.arrivalTime;
+  const flight = flights.find(
+    (flight: Flight) => flight.flightNumber === flightId
+  );
+  if (flight) {
+    flight.arrivalTime = updatedFlightArrivalTime;
+  }
+  res.send(flight);
+});
+
+app.delete("/flights/:flightNumber", (req, res) => {
+  const flightNumber = req.params.flightNumber;
+  flights = flights.filter(
+    (flight: Flight) => flight.flightNumber !== flightNumber
+  );
+  res.send(flights);
+});
+
+app.listen(3032, () => console.log("Server running on port 3032"));
