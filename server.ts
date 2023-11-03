ba70d1ad9e622e62ae5b2abfd7c1f7ab73066de6
Emre Bilge <105619318+emrebilge@users.noreply.github.com>
2023-11-03 11:48:04 -0500

    Emre

--- server.ts
+++ server.ts
@@ -18,4 +18,29 @@ app.get("/", (req, res) => {
   res.send({ message: "Hello World!" });
 });
 
+app.get("/flights", (req, res) => {
+  res.send(flights);
+});
+
+app.post("/flights", (req, res) =>{
+  const newFlight = req.body;
+  flights.push(newFlight);
+  res.send(flights);
+});
+
+app.get("/flights/:flightNumber", (req, res) => {
+  const flightNumber = req.params.flightNumber;
+  res.send(flights.find((flight) => flight.flightNumber === flightNumber));
+});
+
+app.patch("/flights/arrivalTime/:id", (req, res) => {
+    const arrivalTime = req.params.id;
+    const flight = flights.find((flight) => flight.arrivalTime === arrivalTime);
+});
+
+app.delete("/flights/:id", (req, res) => {
+  const flightNumber = req.params.id;
+  flights = flights.filter((flight) => flight.flightNumber !== flightNumber);
+});
+
 app.listen(3030, () => console.log("Server running on port 3030"));
