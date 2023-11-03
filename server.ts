17ce7321d04c6b4a0b6491931a527b1da7ae5151
Neelasha Bhattacharjee <neelashab@gmail.com>
2023-11-02 21:57:34 -0500

    Neelasha workshop 3

--- server.ts
+++ server.ts
@@ -18,4 +18,47 @@ app.get("/", (req, res) => {
   res.send({ message: "Hello World!" });
 });
 
+
+app.get("/flights", (req, res) => {
+  res.send(flights);
+});
+
+app.post("/flights", (req, res) => {
+  req.body.flight;
+});
+
+app.get("/flights/:id", (req, res) => {
+  const flightToFind = req.params.id;
+  const foundFlight = flights.find((flight) => flight.flightNumber === flightToFind)
+
+  if (foundFlight) {
+    res.send(foundFlight);
+  }
+
+  else {
+    console.log("Sorry, flight not found")
+  }
+  
+});
+
+app.patch("/flights/arrivalTime/:id", (req, res) => {
+  const flightToPatch = req.params.id;
+  const foundFlight = flights.find((flight) => flight.flightNumber === flightToPatch)
+  
+  if (foundFlight) { 
+    foundFlight.arrivalTime = "never";
+    res.send(foundFlight)
+  }
+
+  else {
+    console.log("Sorry, flight not found");
+  }
+});
+
+app.delete("/flights/:id", (req, res) => {
+  const flightToDelete = req.params.id;
+  const newFlights = flights.filter((flight) => flight.flightNumber !== flightToDelete);
+  res.send(newFlights);
+})
+
 app.listen(3030, () => console.log("Server running on port 3030"));
